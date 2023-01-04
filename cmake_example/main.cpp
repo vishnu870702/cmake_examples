d894f18ee759834869fcf574a10870da4bbdd46d
dev-shark <viacheslav.sharkovskii@ya.se>
2023-01-04 22:23:23 +0100

    add workflow
    and update an executable file to close without waiting for user input

--- cmake_example/main.cpp
+++ cmake_example/main.cpp
@@ -4,7 +4,9 @@ int main(int argc, char* argv[]) {
     std::cout << "Hello World" << std::endl;
 
     //clear the input stream and wait for any key press.
-    std::cin.sync();
-    std::cin.get();
+    
+    //Comment it out to be able to run the executable file in Github Actions :)
+    // std::cin.sync();
+    // std::cin.get();
     return 0;
 }
